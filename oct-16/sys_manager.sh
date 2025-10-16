1c1c227025eac5c90c1bc29aac4f4761fd66c0d0
v <vaibhavmalik1127@gmail.com>
2025-10-16 10:03:14 +0530

    added add_users and projects creation

---
+++ oct-16/sys_manager.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+case "$1" in
+    "add_users")
+        if [ "$EUID" -ne 0 ]; then
+            echo -e "gib me root bruh${NC}"
+            exit 1
+        fi
+
+        file="$2"
+        if [ ! -f "$file" ]; then
+            echo -e "you sure you passed the correct file??? $file${NC}"
+            exit 1
+        fi
+
+        created=0
+        exists=0
+
+        while read username; do
+            if [ -z "$username" ]; then
+                continue
+            fi
+
+            if id "$username" >/dev/null 2>&1; then
+                echo -e "$username already exists${NC}"
+                ((exists++))
+            else
+                useradd -m "$username"
+                if [ $? -eq 0 ]; then
+                    echo -e "$created $username${NC}"
+                    ((created++))
+                else
+                    echo -e "$uh oh, that didnt work out to create $username${NC}"
+                fi
+            fi
+        done < "$file"
+
+        echo ""
+        echo "created these new users: $created"
+        echo "these users already existed: $exists"
+        ;;
+
+    "setup_projects")
+        if [ "$EUID" -ne 0 ]; then
+            echo -e "$root for this${NC}"
+            exit 1
+        fi
+
+        username="$2"
+        num_projects="$3"
+
+        if ! id "$username" >/dev/null 2>&1; then
+            echo -e "$User $username doesn't exist${NC}"
+            exit 1
+        fi
+
+        base_dir="/home/$username/projects"
+        mkdir -p "$base_dir"
+
+        for i in $(seq 1 $num_projects); do
+            project_dir="$base_dir/project$i"
+            mkdir -p "$project_dir"
+
+            echo "project$i" > "$project_dir/README.txt"
+            echo "blablabalhuhnaheutnahtnh"  >> "$project_dir/README.txt"
+            echo "created: $(date)" >> "$project_dir/README.txt"
+            echo "user: $username" >> "$project_dir/README.txt"
+
+            chown -R "$username:$username" "$project_dir"
+            chmod 755 "$project_dir"
+            chmod 640 "$project_dir/README.txt"
+
+            echo -e "created some $project_dir${NC}"
+        done
+
+        echo -e "created the  projects for $username${NC}"
+        ;;
+
+    "help"|*)
+        echo "Usage:"
+        echo "  $0 add_users <file>"
+        echo "  $0 setup_projects <username> <number>"
+        echo "  $0 help"
+        ;;
+esac
